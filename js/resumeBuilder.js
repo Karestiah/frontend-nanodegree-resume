3e32e221dd3c0d8679815090875b6152fadea335
Karestiah <Klawson@alpha.yearup.org>
2016-12-29 14:41:12 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var Karestiah
+ var intern
+ var Karestiah = HTMLheaderName.replace("%data%",name);
+ $("#header"),append(formattedName);
+ ("header"),prepend(Karestiah);
+ var Karestiah = HTMLheaderName.replace("%data%",role);
